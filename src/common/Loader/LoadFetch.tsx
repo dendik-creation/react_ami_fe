ee70b05843fe438ac3477b9042678400dee0a60e
dendik-creation <setiawandendik0205@gmail.com>
2024-05-14 13:11:29 +0700

    Add GITIGNORE

--- src/common/Loader/LoadFetch.tsx
+++ src/common/Loader/LoadFetch.tsx
@@ -8,23 +8,23 @@ export default function LoadFetch() {
   const [timeoutValue, setTimeoutValue] = useState<number>(10000);
   const [intervalValue, setIntervalValue] = useState<number>(0);
   let intervalLoop: any;
-  useEffect(() => {
-    if (intervalValue == timeoutValue) {
-      toastFire({
-        message: 'Data Sulit Dimuat, Refresh Halaman & Coba Lagi',
-        status: false,
-      });
-      clearInterval(intervalLoop);
-    }
-  }, [intervalValue]);
-  const startInterval = () => {
-    intervalLoop = setInterval(() => {
-      setIntervalValue((prev: number) => prev + 100);
-    }, 100);
-  };
-  useEffect(() => {
-    startInterval();
-  }, []);
+  // useEffect(() => {
+  //   if (intervalValue == timeoutValue) {
+  //     toastFire({
+  //       message: 'Data Sulit Dimuat, Refresh Halaman & Coba Lagi',
+  //       status: false,
+  //     });
+  //     clearInterval(intervalLoop);
+  //   }
+  // }, [intervalValue]);
+  // const startInterval = () => {
+  //   intervalLoop = setInterval(() => {
+  //     setIntervalValue((prev: number) => prev + 100);
+  //   }, 100);
+  // };
+  // useEffect(() => {
+  //   startInterval();
+  // }, []);
   return (
     <>
       <div className="w-full h-full flex justify-center items-center">
